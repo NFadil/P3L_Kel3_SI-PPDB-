d3df952bfae61d0ed630a4df28732c9009b1c044
NFadil <75855353+NFadil@users.noreply.github.com>
2023-04-17 22:13:12 +0700

    Memasukan database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://P3L_Kel3_SI-PPDB-';
 
 /*
 |--------------------------------------------------------------------------
